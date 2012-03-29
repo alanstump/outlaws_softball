bb24738b388f709279d59f179b0c987c79a2d235
Alan Stump <alstump@umail.iu.edu>
2012-03-28 21:18:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OutlawsSoftball::Application.config.secret_token = 'ea74d885d7a7e6c679fc1a270fc0ac6996dcaffd1ba2b42815c4f7981e4dd21aaa7fd3cd328b38d722eb0d7d5974cde0f2bae9f42c03fc52b84414e1ed31fae9'
